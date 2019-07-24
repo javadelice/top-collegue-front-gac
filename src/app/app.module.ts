5c8be53b5b71252c713b97a6cc39bd11d20f3a92
Adrien CHAUVIN <adrienchauvin44@gmail.com>
2019-07-24 17:37:58 +0200

    Ajout des composants, routage, dataService, authService et autres configs de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VotesComponent } from './votes/votes.component';
+import { ClassementComponent } from './classement/classement.component';
+import { ConnexionGuard } from './guards/ConnexionGuard';
+
+const routes: Routes = [
+  { path: 'connexion', component: ConnectionComponent },
+  {
+    path: '',
+    canActivate: [ConnexionGuard],
+    children: [
+      { path: 'votes', component: VotesComponent },
+      { path: 'classement', component: ClassementComponent }]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent,
+    NavbarComponent,
+    VotesComponent,
+    ClassementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
