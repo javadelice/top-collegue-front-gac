2a5d18233ca8e9ff79936877eded208381df675e
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-25 14:28:19 +0200

    Composant classement #6

--- src/app/classement/classement.component.ts
+++ src/app/classement/classement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+import { CandidatClassement } from '../models/CandidatClassement';
 
 @Component({
   selector: 'app-classement',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassementComponent implements OnInit {
 
-  constructor() { }
+  public classement: CandidatClassement[] = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getClassement().subscribe(classement => {
+      this.classement = classement;
+      console.log(classement);
+    });
   }
 
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { CandidatClassement } from '../models/CandidatClassement';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +10,15 @@ import { Injectable } from '@angular/core';
 
 export class DataService {
 
+  BACKEND_URL = environment.backendUrl;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getClassement(): Observable<CandidatClassement[]> {
+    return this.http.get<CandidatClassement[]>(this.BACKEND_URL + '/classement', {withCredentials: true});
+  }
 
 
 }
